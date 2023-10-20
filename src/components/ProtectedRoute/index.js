91d45365355c4016cf6d10a7780dc4ac913c3c16
Nakkatarun <120617341+Nakkatarun@users.noreply.github.com>
2023-10-20 15:19:14 +0530

    Update index.js

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,7 +1,7 @@
 import {Redirect, Route} from 'react-router-dom'
 import Cookie from 'js-cookie'
 
-const ProtectedRoute = props => {
+const ProtectedRoute = props => {              // here validate the user and then permit the user to enter the next pages or not
   const token = Cookie.get('jwt_token')
 
   if (token === undefined) {
