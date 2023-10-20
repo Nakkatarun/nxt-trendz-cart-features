94ced689d9a75b1a465d6b6b5ea17a84f3e7bd31
Nakkatarun <120617341+Nakkatarun@users.noreply.github.com>
2023-10-20 15:20:58 +0530

    Update CartContext.js

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const CartContext = React.createContext({
+const CartContext = React.createContext({          // we can use the context for simple pass the props throughout the all routes 
   cartList: [],
   removeAllCartItems: () => {},
   addCartItem: () => {},
