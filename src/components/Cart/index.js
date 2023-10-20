776ad209f105107c8888eefb70c1f3a0f6f91a8f
Nakkatarun <120617341+Nakkatarun@users.noreply.github.com>
2023-10-20 14:52:25 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -6,8 +6,8 @@ import EmptyCartView from '../EmptyCartView'
 
 import './index.css'
 
-const Cart = () => (
-  <CartContext.Consumer>
+const Cart = () => (    // cart view display the number of items should be select to the cart
+  <CartContext.Consumer>     // use context to avoid the prop drilling 
     {value => {
       const {cartList} = value
       const showEmptyView = cartList.length === 0
